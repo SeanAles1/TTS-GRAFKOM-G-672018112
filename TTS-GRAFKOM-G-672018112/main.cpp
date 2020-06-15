110eb5f016b70f7e73ab5ed1504d27ee58e7d570
SeanAles1 <672018112@student.uskw.edu>
2020-06-15 20:16:22 +0700

    Mendeklarasikan Variabel x dan y, Menambahkan Pola 1 dan Pola 2

--- TTS-GRAFKOM-G-672018112/main.cpp
+++ TTS-GRAFKOM-G-672018112/main.cpp
@@ -1,8 +1,98 @@
 #include <iostream>
 #include <GL/freeglut.h>
 
-void Render() {
+int x = 0;
+int y = 0;
 
+void Pola1() {
+	glColor3ub(255, 238, 204);
+	glBegin(GL_POLYGON);
+	//Kotak Putih
+	glVertex2f(51 + x, 121 + y);
+	glVertex2f(86 + x, 51 + y);
+	glVertex2f(151 + x, 86 + y);
+	glVertex2f(116 + x, 151 + y);
+	glEnd();
+
+	//Kotak - Hitam
+	glColor3ub(0, 0, 0);
+	glBegin(GL_POLYGON);
+
+	glVertex2f(86 + x, 85 + y);
+	glVertex2f(86 + x, 121 + y);
+	glVertex2f(116 + x, 121 + y);
+	glVertex2f(116 + x, 85 + y);
+	glEnd();
+
+	//Garis
+	glColor3ub(0, 0, 0);
+	glBegin(GL_LINES);
+	//1
+	glVertex2f(86 + x, 121 + y);
+	glVertex2f(51 + x, 121 + y);
+	//2
+	glVertex2f(87 + x, 51 + y);
+	glVertex2f(87 + x, 85 + y);
+	//3
+	glVertex2f(151 + x, 86 + y);
+	glVertex2f(116 + x, 86 + y);
+	//4
+	glVertex2f(116 + x, 151 + y);
+	glVertex2f(116 + x, 121 + y);
+
+	glEnd();
+}
+
+void Pola2() {
+	glColor3ub(255, 238, 204);
+	glBegin(GL_POLYGON);
+
+	//Kotak Putih
+	glVertex2f(151 + x, 86 + y);
+	glVertex2f(216 + x, 51 + y);
+	glVertex2f(251 + x, 121 + y);
+	glVertex2f(186 + x, 151 + y);
+
+	glEnd();
+
+	//Kotak - Hitam
+	glColor3ub(0, 0, 0);
+	glBegin(GL_POLYGON);
+
+	glVertex2f(186 + x, 85 + y);
+	glVertex2f(186 + x, 121 + y);
+	glVertex2f(216 + x, 121 + y);
+	glVertex2f(216 + x, 85 + y);
+	glEnd();
+
+	//Garis
+	glColor3ub(0, 0, 0);
+	glBegin(GL_LINES);
+
+	//1
+	glVertex2f(151 + x, 86 + y);
+	glVertex2f(186 + x, 86 + y);
+	//2
+	glVertex2f(216 + x, 51 + y);
+	glVertex2f(216 + x, 85 + y);
+	//3
+	glVertex2f(251 + x, 121 + y);
+	glVertex2f(216 + x, 121 + y);
+	//4
+	glVertex2f(187 + x, 151 + y);
+	glVertex2f(187 + x, 121 + y);
+
+	glEnd();
+}
+
+void Render() {
+	glClearColor(0, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	Pola1();
+	Pola2();
+	
+	glFlush();
 }
 
 int main(int argc, char** argv) {
